26b956f14ec27b8182b03f30d759f28a4ef31809
BrentHarshbarger <67839293+BrentHarshbarger@users.noreply.github.com>
2020-07-30 00:44:57 -0400

    Fixed overloading

--- Lab4Exercise1/src/Box.java
+++ Lab4Exercise1/src/Box.java
@@ -1,57 +1,61 @@
 
 public class Box {
-		// Required for the entire class of Box
-		static int size = 0;
-		static char alt2 = 'c';
-		static char alt = '*';
-		
-		public static void main(String[] args) {
-			// Lewis University - Programming Fundamentals Lab 4 Exercise 1
-			// Brent Harshbarger
+	// Required for the entire class of Box
+	int size = 0;
+	
 
-			// --------------------------------------------------------------------------------------------------------------
-			// The goal of this program is to pass a box size from main to the printBox for
-			// printing to the screen
-			// -------------------------------------------------------------------------------------------------------------
+	public static void main(String[] args) {
+		// Lewis University - Programming Fundamentals Lab 4 Exercise 1
+		// Brent Harshbarger
 
-			Box box = new Box();
-			Box.size = 5; // Sets the rows and columns of the box.printBox
-			box.printBox(size, alt, alt2);
+		// --------------------------------------------------------------------------------------------------------------
+		// The goal of this program is to pass a box size from main to the printBox for
+		// printing to the screen
+		// -------------------------------------------------------------------------------------------------------------
+
+
+		Box box = new Box();
+
+		box.size = 5; // Sets the rows and columns of the box.printBox
 		
+		box.printBox();
 		
-		
-		
+		System.out.println();
+		box.printBox('c');
+	
+
+	}
+
+	public void printBox() {
+		// The outer loop prints the rows of the box
+		for (int row = 1; row <= size; row++) {
+
+			// The inner loop prints the columns in each row
+			for (int column = 1; column <= size; column++) {
+				System.out.print("*");
+			}
+
+			// This advances the line to print the next row
+			System.out.println();
+
 		}
-
-		public void printBox(int size, char alt, char alt2) {
+	}
+	
+	public void printBox (char c) {
 		
-			
-			// The outer loop prints the rows of the box
-			for (int row = 1; row <= size; row++) {
+		
+		// The outer loop prints the rows of the box
+		for (int row = 1; row <= size; row++) {
 
-				// The inner loop prints the columns in each row
-				for (int column = 1; column <= size; column++) {
-					
-					System.out.print(alt);
-					
-				}
-
-				// This advances the line to print the next row
-				System.out.println();
-
+			// The inner loop prints the columns in each row
+			for (int column = 1; column <= size; column++) {
+				System.out.print("c");
 			}
-			for (int row = 1; row <= size; row++) {
 
-				// The inner loop prints the columns in each row
-				for (int column = 1; column <= size; column++) {
-					
-					System.out.print(alt2);
-					
-				}
+			// This advances the line to print the next row
+			System.out.println();
 
-				// This advances the line to print the next row
-				System.out.println();
-
-			}
-		} 
+		}
+	}
+	
 }
